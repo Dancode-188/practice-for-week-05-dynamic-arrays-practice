2a4f8d128038d97e8d9942dee28b05e4ad95d5ce
Dancode-188 <danbitengo@gmail.com>
2024-09-24 23:02:29 +0300

    Refactor DynamicArray class to implement unshift() method and resize() method

--- solution/dynamic-array.js
+++ solution/dynamic-array.js
@@ -1,19 +1,33 @@
 class DynamicArray {
 
   constructor(defaultSize=4) {
-
-    // Your code here
-
+    this.length = 0;
+    this.capacity = defaultSize;
+    this.data = new Array(defaultSize);
   }
 
   read(index) {
-
-    // Your code here
+    return this.data[index];
   }
 
   unshift(val) {
+    if (this.length === this.capacity) {
+      this.resize();
+    }
+    for (let i = this.length; i > 0; i--) {
+      this.data[i] = this.data[i - 1];
+    }
+    this.data[0] = val;
+    this.length++;
+  }
 
-    // Your code here
+  resize() {
+    this.capacity *= 2;
+    const newData = new Array(this.capacity);
+    for (let i = 0; i < this.length; i++) {
+      newData[i] = this.data[i];
+    }
+    this.data = newData;
   }
 
 }
